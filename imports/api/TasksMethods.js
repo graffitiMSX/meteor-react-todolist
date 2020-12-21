f936f4459cdc8be365a34644da2d4b6a7c943489
Rudolf Arthur Frans Gultich <rfrans.3ste@sofisa.com.br>
2020-12-21 18:57:29 -0300

    Publications

--- imports/api/TasksMethods.js
+++ imports/api/TasksMethods.js
@@ -23,6 +23,12 @@ Meteor.methods({
       throw new Meteor.Error('Not authorized.');
     }
 
+    const task = TasksCollection.findOne({ _id: taskId, userId: this.userId });
+
+    if (!task) {
+      throw new Meteor.Error('Access denied.');
+    }
+
     TasksCollection.remove(taskId);
   },
 
@@ -34,10 +40,16 @@ Meteor.methods({
       throw new Meteor.Error('Not authorized.');
     }
 
+    const task = TasksCollection.findOne({ _id: taskId, userId: this.userId });
+
+    if (!task) {
+      throw new Meteor.Error('Access denied.');
+    }
+
     TasksCollection.update(taskId, {
       $set: {
-        isChecked
-      }
+        isChecked,
+      },
     });
-  }
+  },
 });
--- imports/ui/TaskForm.jsx
+++ imports/ui/TaskForm.jsx
@@ -1,5 +1,4 @@
 import React, { useState } from 'react';
-import { TasksCollection } from '../db/TasksCollection';
 
 export const TaskForm = ({ user }) => {
   const [text, setText] = useState('');
